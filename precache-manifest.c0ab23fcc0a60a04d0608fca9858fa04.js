dad13f631e8cad9d65b07f6d9d410c93674e0f02
root <root@dante.dante>
2020-08-28 14:58:46 +0530

    Updates

---
+++ precache-manifest.c0ab23fcc0a60a04d0608fca9858fa04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9af25ffa2147afc1ff23493cda2b21",
+    "url": "/Algo-Visualizer/index.html"
+  },
+  {
+    "revision": "8f103456fa050429bdb2",
+    "url": "/Algo-Visualizer/static/css/2.09d1976d.chunk.css"
+  },
+  {
+    "revision": "f580b34406e257809ecd",
+    "url": "/Algo-Visualizer/static/css/main.f67cedb0.chunk.css"
+  },
+  {
+    "revision": "8f103456fa050429bdb2",
+    "url": "/Algo-Visualizer/static/js/2.f7b60775.chunk.js"
+  },
+  {
+    "revision": "c212a9ec01f2447eb77909d3d2a5af5a",
+    "url": "/Algo-Visualizer/static/js/2.f7b60775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f580b34406e257809ecd",
+    "url": "/Algo-Visualizer/static/js/main.80a303fe.chunk.js"
+  },
+  {
+    "revision": "7407d09076d67f0ee2dc",
+    "url": "/Algo-Visualizer/static/js/runtime-main.2d6c2b82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Algo-Visualizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
